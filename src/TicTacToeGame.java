0644d93bc08e517d3e8542a1e1fd499cc7065dbb
LuisLeal123 <lal10@rice.edu>
2023-08-19 02:33:56 -0500

    -commit

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -20,6 +20,7 @@ public class TicTacToeGame extends JFrame {
         setVisible(true);
     }
 
+
     private void initializeButtons() {
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
